ff393d8395f0ab91af915678119e505c30203a04
Rahul Rana <me.rahulrana@outlook.com>
2025-01-04 02:21:07 +0000

    fix: change name

--- data/index.ts
+++ data/index.ts
@@ -118,27 +118,27 @@ export const projects = [
 
 export const testimonials = [
 	{
-		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		quote: "Collaborating with XXXX was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. XXXX's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, XXXX is the ideal partner.",
 		name: 'Michael Johnson',
 		title: 'Director of AlphaStream Technologies',
 	},
 	{
-		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		quote: "Collaborating with XXXX was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. XXXX's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, XXXX is the ideal partner.",
 		name: 'Michael Johnson',
 		title: 'Director of AlphaStream Technologies',
 	},
 	{
-		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		quote: "Collaborating with XXXX was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. XXXX's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, XXXX is the ideal partner.",
 		name: 'Michael Johnson',
 		title: 'Director of AlphaStream Technologies',
 	},
 	{
-		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		quote: "Collaborating with XXXX was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. XXXX's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, XXXX is the ideal partner.",
 		name: 'Michael Johnson',
 		title: 'Director of AlphaStream Technologies',
 	},
 	{
-		quote: "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		quote: "Collaborating with XXXX was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. XXXX's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, XXXX is the ideal partner.",
 		name: 'Michael Johnson',
 		title: 'Director of AlphaStream Technologies',
 	},
